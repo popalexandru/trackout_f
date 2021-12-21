1fdf920964b622fa822ee9a55fbd075b72702c9c
popalexandru <popalexandru1197@gmail.com>
2021-12-21 13:34:16 +0200

    Saving before miniplayer

--- lib/main.dart
+++ lib/main.dart
@@ -3,54 +3,43 @@ import 'package:miniplayer/miniplayer.dart';
 import 'package:trackout_f/screens/examples_screen.dart';
 import 'package:trackout_f/screens/home_screen.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      routes: {
-        '/': (context) => const Home(),
-        '/examples': (context) => const ExampleScreen()
-      },
-    ),
+void main() => runApp(MyApp());
 
-/*      MaterialApp(
-    home: Container(
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              Expanded(
-                child: Stack(
-                  children: [
-                    SizedBox.expand(child: Home()),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Miniplayer(
-                        minHeight: 70,
-                        maxHeight: 370,
-                        builder: (height, percentage) {
-                          return Center(
-                            child: Text('$height, $percentage'),
-                          );
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Miniplayer example',
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-    ),
-  )*/
-/*    Miniplayer(
-      minHeight: 70,
-      maxHeight: 370,
-      builder: (height, percentage) {
-        return Center(
-          child: Text('$height, $percentage'),
+      builder: (context, child) { // <--- Important part
+        return Stack(
+          children: [
+            Home(),
+            Offstage(
+              offstage: false,
+              child: Miniplayer(
+                minHeight: 70,
+                maxHeight: MediaQuery.of(context).size.height*0.75,
+                builder: (height, percentage) {
+                  if(percentage > 0.2)
+                    return Text('maximized');
+                  else
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: Colors.indigo
+                      ),
+                      child: Center(
+                        child: Text('Ongoing workout', style: TextStyle(fontWeight: FontWeight.bold),),
+                      ),
+                    );
+                },
+              ),
+            ),
+          ],
         );
       },
-    )*/
-      );
+    );
+  }
 }
